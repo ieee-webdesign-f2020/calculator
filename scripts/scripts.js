de185c9258520fb6734af1d8488c67966d946fcf
Sammy <sumezawa@protonmail.com>
2020-11-07 16:24:22 -0800

    working arithmetic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,60 @@
 /* scripts.js */
 
+var operator;
+var ar_input1;
+var ar_input2;
+
+function add() {
+    operator = 0;
+    arithmetic();
+}
+
+
+function sub() {
+    operator = 1;
+    arithmetic();
+}
+
+function mult() {
+    operator = 2;
+    arithmetic();
+}
+
+function div() {
+    operator = 3;
+    arithmetic();
+}
+
+function mod() {
+    operator = 4;
+    arithmetic();
+}
+
 function arithmetic() {
-    input1 = document.getElementById("ar_input1");
-    input2 = document.getElementById("ar_input2");
-    operator = document.getElementById("operator");
+    ar_input1 = document.getElementById("ar_input1");
+    ar_input2 = document.getElementById("ar_input2");
+    ar_output = document.getElementById("ar_output");
     switch(operator) {
-        case "+":
-            ar_output.innerHTML = parseInt(input1) + parseInt(input2);
-            break;
-        case "-":
-            ar_output.innerHTML = parseInt(input1) - parseInt(input2);
-            break; 
-        case "*":
-            ar_output.innerHTML = parseInt(input1) * parseInt(input2);
-            break;
-        case "/":
-            ar_output.innerHTML = parseInt(input1) / parseInt(input2);
-            break;
-        case "%":
-            ar_output.innerHTML = parseInt(input1) % parseInt(input2);
-            break;
-        default:
-            ar_output.innerHTML = 0;
+    case 0:
+        ar_output.innerHTML = parseInt(ar_input1.value) + parseInt(ar_input2.value);
+        break;
+    case 1:
+        ar_output.innerHTML = parseInt(ar_input1.value) - parseInt(ar_input2.value);
+        break; 
+    case 2:
+        ar_output.innerHTML = parseInt(ar_input1.value) * parseInt(ar_input2.value);
+        break;
+    case 3:
+        ar_output.innerHTML = parseInt(ar_input1.value) / parseInt(ar_input2.value);
+        break;
+    case 4:
+        ar_output.innerHTML = parseInt(ar_input1.value) % parseInt(ar_input2.value);
+        break;
+    default:
+        ar_output.innerHTML = 0;
     }
 }
+
 
 function trig() {
 }
